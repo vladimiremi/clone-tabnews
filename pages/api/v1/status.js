eb976639a56b5b17bec2e1eec2c6dca39cef3555
Vladimir Costa <41305527+vladimiremi@users.noreply.github.com>
2024-01-09 02:44:09 +0000

    add  `database.js` and `.env` files

--- pages/api/v1/status.js
+++
@@ -1,5 +0,0 @@
-function status(request, response) {
-  response.status(200).send({ hello: "Hello World!" });
-}
-
-export default status;
---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,9 @@
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).send({ hello: "Hello World!" });
+}
+
+export default status;
